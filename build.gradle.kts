cbe15f2f738deb64ca7ad11d7c70f2a0f3150df9
Leonid <50008736+lolkek22854@users.noreply.github.com>
2025-04-25 22:32:32 +0300

    quick fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,9 +43,9 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
+//tasks.withType<KotlinCompile> {
+//    kotlinOptions.jvmTarget = "1.8"
+//}
 
 application {
     mainClass.set("MainKt")
