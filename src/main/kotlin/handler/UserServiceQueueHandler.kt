813ce111733d6eff440b6a0aea4ad5a8f39b0f13
Leonid <50008736+lolkek22854@users.noreply.github.com>
2025-04-29 23:21:33 +0300

    changed serial name for request

--- src/main/kotlin/handler/UserServiceQueueHandler.kt
+++ src/main/kotlin/handler/UserServiceQueueHandler.kt
@@ -12,6 +12,7 @@ import java.util.UUID
 @Serializable
 data class ProductListRequest(
     @Contextual val requestId: UUID,
+    @SerialName("requestType")
     val type: String,
     val ids: List<Int>
 )
